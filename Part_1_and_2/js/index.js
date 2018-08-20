0328e92542138020061bca2da5da6c8ce0eba22d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 04:20:56 -0500

    created function that finds first number divisible by 7 that is greater than or equal to 20

--- Part_1_and_2/js/index.js
+++ Part_1_and_2/js/index.js
@@ -1,13 +1,24 @@
 console.log('Part 1 & 2');
 
 // Closures
-
+// A function that close over a local variable is called a closure
 const multiplier = (factor) => {
 	return (num) => {
 		return num * factor;
 	};
 };
-
 const twice = multiplier(2);
-
 console.log(twice(10)); // 20
+// All though the function call that created the local variable (factor) no longer exist
+// We are able to reference the instance of a local variable in an enclosing function because of Closures.
+
+
+
+
+// First number greater than or equal to 20 that is divisible by 7
+for (let i = 20; ; i ++) {
+	if (i % 7 === 0) {
+		console.log(i);
+		break;
+	}
+}
