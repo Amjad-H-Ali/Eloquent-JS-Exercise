5c42f31335e7732b73e93904c17a5500e06561d7
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-20 04:12:55 -0500

    Created function that demonstrates Closure

--- Part_1_and_2/js/index.js
+++ Part_1_and_2/js/index.js
@@ -1,1 +1,13 @@
 console.log('Part 1 & 2');
+
+// Closures
+
+const multiplier = (factor) => {
+	return (num) => {
+		return num * factor;
+	};
+};
+
+const twice = multiplier(2);
+
+console.log(twice(10)); // 20
